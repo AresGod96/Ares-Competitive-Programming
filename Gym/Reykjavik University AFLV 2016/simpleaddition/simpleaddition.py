d0d28e7ccf62a4bc93a2d7997ffe192733770bb0
Minh Tu Nguyen <ngminhtu96@gmail.com>
2023-04-14 15:16:05 +0900

    Add AFLV 2016 solutions

---
+++ Gym/Reykjavik University AFLV 2016/simpleaddition/simpleaddition.py
@@ -0,0 +1,9 @@
+"""""
+	- author: Ares
+	- "Let's make it to ORANGE!"
+	- Problem: Reykjavík University ÁFLV 2016 - simpleaddition
+	- Problem link: https://open.kattis.com/problems/simpleaddition
+"""""
+a = int(input())
+b = int(input())
+print(a + b)
